6497d47ee0badac9ee61d76e01c28fe07e9dbfda
Jo Ee <joeechua123@gmail.com>
2021-02-24 23:02:50 +0800

    index files updated, style2.css added

--- app.py
+++ app.py
@@ -10,25 +10,37 @@ app = Flask(__name__)
 
 bangsa = ['MELAYU', 'CINA', 'INDIA', 'LAIN-LAIN']
 
-@app.route('/race', methods = ['GET'])
+@app.route('/api/race', methods = ['GET'])
 def get_races():
     return {'races': bangsa}
 
-@app.route("/", methods = ['GET'])
+@app.route("/")
 def hello():
     return render_template('index2.html')
 
-@app.route("/race/<name>", methods = ['GET'])
-def get(name):
-    index = model.predict([name])[0]
-    p_race = bangsa[index]
-    return {'predictedrace': p_race}
+@app.route("/api/race/<name>", methods = ['GET'])
+def get_race(name):
+    if request.method =='GET':
+        index = model.predict([name])[0]
+        p_race = bangsa[index]
+        return {'predictedrace': p_race}
+    else:
+        return {'message': 'incorrect request method'}
+    
+@app.route("/api", methods = ['GET'])
+def explain_api():
+    return render_template('index.html')
     
 @app.route("/race/predict", methods = ['GET'])
 def pred_race():
-   name = (request.args['Pname']).lower()
-   race_index = model.predict([name])[0]
-   return render_template('result.html', prediction=race_index)
+   if request.method == 'GET':
+       name = (request.args['Pname']).lower()
+       race_index = (model.predict([name]))[0]
+       probs = model.predict_proba([name])[0]
+       maxi = np.where(probs == np.max(probs))[0][0]
+       return render_template('result.html', prediction=race_index, m = probs[0], c = probs[1], i = probs[2] ,l = probs[3], maxim = maxi)
+   else:
+       return {'message': 'incorrect request method'}
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
